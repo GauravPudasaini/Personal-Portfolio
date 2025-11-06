12757eea06de9dc7239ebc5352d2e134d212d7c7
a <gauravpudasaini11@gmail.com>
2025-11-06 23:25:53 +0545

    Pages Added for Portfolio

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+ 
+
+const Navbar = () => {
+    return (
+        <>
+        <div className="fixed">
+            <nav>
+                <ul>
+                    <li> </li>
+                    <li> </li>
+                    <li> </li>
+                    <li> </li>
+                </ul>
+            </nav>
+        </div>
+        </>
+    )
+}
